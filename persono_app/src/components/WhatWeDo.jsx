acc37f1173e5c7c6473e74fe593defbd4e633bfc
Jeffrey Qin <jeffrey.qin22@gmail.com>
2024-02-21 22:05:55 -0800

    CTA and Download component redesign

--- persono_app/src/components/WhatWeDo.jsx
+++ persono_app/src/components/WhatWeDo.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import './WhatWeDo.css';
-import pillowImage from '../assets/vertical1.jpg'; // Adjust the import path as necessary
+import pillowImage from '../assets/vertical1.jpg';
 
 const WhatWeDo = () => {
-    return (
-      <section className="what-we-do">
-        <h2 className="section-title">What Do We Do?</h2> {/* Added class for styling */}
-        <div className="container">
-          <div className="buttons-section">
-            <button>Take Our Quiz</button>
-            <button>Download Our App</button>
-            <button>About Us</button>
-          </div>
-          <div className="info-section">
-            <p>Information about the pillow...</p>
-          </div>
-          <div className="image-section">
-            <img src={pillowImage} alt="Pillow" />
+  return (
+    <section className="what-we-do">
+      <h2>What Do We Do?</h2>
+      <div className="content-container">
+        <div className="text-and-buttons">
+        <p className="bordered-paragraph">Discover how Persono improves your sleep with a personalized pillow, tailored to your sleep habits.</p>
+          <div className="buttons">
+            <div className="tooltip-container">
+              <button className="quiz-btn">Take Our Quiz</button>
+              <span className="tooltip-content">Find the perfect pillow for you!</span>
+            </div>
+            <div className="tooltip-container">
+              <button className="app-btn">Download Our App</button>
+              <span className="tooltip-content">Get a personalized sleeping experience.</span>
+            </div>
+            <div className="tooltip-container">
+              <button className="about-btn">About Us</button>
+              <span className="tooltip-content">Learn more about our mission and team.</span>
+            </div>
           </div>
         </div>
-      </section>
-    );
-  };
-  
-  export default WhatWeDo;
+        <div className="image">
+          <img src={pillowImage} alt="Pillow" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WhatWeDo;
