183458ae6f1ad909777b41502c6833e62d927aaa
Jeffrey Qin <jeffrey.qin22@gmail.com>
2024-03-17 19:26:56 -0700

    minor padding changes

--- persono_app/src/App.jsx
+++ persono_app/src/App.jsx
@@ -33,8 +33,8 @@ function App() {
               <PillowFeatures />
               <TrackerFeatures />
               <AppFeatures />
-              <Unique />
               <Interlude />
+              <Unique />
               <Interlude2 />
               <Steps />
               <MoreInfo />
